52caea22ccb4979b57abc78d3625d3e18d95d92f
Brandon DeRosier <x@bdero.me>
2016-05-02 16:24:10 -0400

    Added player movement

---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+
+	public float torqueSpeed;
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 torque = new Vector3 (Input.GetAxis ("Vertical"), 0f, -Input.GetAxis ("Horizontal"));
+		rb.AddTorque (torque * torqueSpeed);
+	}
+}
