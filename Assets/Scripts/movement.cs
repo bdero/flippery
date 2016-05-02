2d534f5cad834aa7bb0ad8031d13fb7af9c3f6e1
Brandon DeRosier <x@bdero.me>
2016-05-02 19:49:34 -0400

    Improve movement using forces

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -4,17 +4,35 @@ using System.Collections;
 public class movement : MonoBehaviour {
 
 	public float torqueSpeed;
+	public float movementSpeed;
 
 	private Rigidbody rb;
+	private bool colliding = false;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
 	}
-	
+
+	void OnCollisionEnter(Collision collision) {
+		colliding = true;
+	}
+
+	void OnCollisionExit(Collision collision) {
+		colliding = false;
+	}
+
 	// Update is called once per frame
 	void FixedUpdate () {
-		Vector3 torque = new Vector3 (Input.GetAxis ("Vertical"), 0f, -Input.GetAxis ("Horizontal"));
-		rb.AddTorque (torque * torqueSpeed);
+		float x = Input.GetAxis ("Horizontal");
+		float y = Input.GetAxis ("Vertical");
+
+		if (colliding) {
+			Vector3 torque = new Vector3 (y, 0f, -x);
+			rb.AddTorque (torque * torqueSpeed);
+		} else {
+			Vector3 movement = new Vector3 (x, 0f, y);
+			rb.AddForce (movement * movementSpeed);
+		}
 	}
 }
